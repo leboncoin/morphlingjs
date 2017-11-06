74ea3fa60311b0048fdd98bad1bfcb0758abe51d
vroudge <v.roudge@gmail.com>
2017-11-06 12:03:40 +0100

    remove console.log

--- src/db/fixtures.js
+++ src/db/fixtures.js
@@ -47,8 +47,6 @@ export const createFixturesFromDefinitions = (db, definitions) => {
  * @returns {{}}
  */
 const generateOneFixture = (db, model) => {
-    console.log(model);
-
     const { type, modelName } = model;
 
     switch (type) {
